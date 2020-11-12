3a6628be7ab683aab51e30b44d183389b74fad30
Bill Mårtensson <bill@magictechnology.se>
2020-11-12 14:14:32 +0100

    Initial Commit

---
+++ SwiftUIIntro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIIntro
+//
+//  Created by Bill Martensson on 2020-11-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
